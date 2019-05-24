eb4cb38c8101d337be8a78549a48193b9409bd36
matnagra <monavarrete@uc.cl>
2019-05-24 17:07:31 -0400

    pedir_stock_minimo_grupos Celery ready

--- proj/api/tasks.py
+++ proj/api/tasks.py
@@ -2,14 +2,16 @@ from __future__ import absolute_import, unicode_literals
 from celery import task
 import requests
 from .funciones_bodega import *
+from datos import *
 
 @task
 def pedir_stock_minimo_grupos():
     pedir = generar_dict_compras()
     liberar_recepcion()
+    # Se revisan los tiempos de los pedidos y se cambia de proveedor en caso de no cumplir
+    pedidos_nuestros_celery_control(pedidos_nuestros)
     for sku, cantidad in pedir.items():
-        pedir_productos_sku(sku, 3)
-        pedir_productos_sku(sku, 3, url_changed=True)
+        iniciar_orden(sku, cantidad, pedidos_nuestros)
         liberar_recepcion()
 
 @task
