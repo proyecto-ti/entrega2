043c296be73b20f4db9683fb89e1ee3b22e4d6cc
Nicolas <nnaguirre@uc.cl>
2019-05-29 22:38:39 -0400

    subiendo

--- proj/api/tasks.py
+++ proj/api/tasks.py
@@ -5,32 +5,32 @@ from .funciones.pedir_grupo import *
 from .funciones.fabricar import *
 from .funciones.ftp import *
 
- @task
- def pedir_stock_minimo_grupos():
-      pedir = generar_dict_compras()
-      liberar_almacen("recepcion")
-       Se revisan los tiempos de los pedidos y se cambia de proveedor en caso de no cumplir
-      for sku, cantidad in pedir.items():
-          pedir_productos_sku(sku, 3)
-          liberar_almacen("recepcion")
+@task
+def pedir_stock_minimo_grupos():
+     pedir = generar_dict_compras()
+     liberar_almacen("recepcion")
+     #Se revisan los tiempos de los pedidos y se cambia de proveedor en caso de no cumplir
+     for sku, cantidad in pedir.items():
+         pedir_productos_sku(sku, 3)
+         liberar_almacen("recepcion")
 
- @task
- def crear_productos():
-        enviar_fabricar()
+@task
+def crear_productos():
+    enviar_fabricar()
 
- @task
- def pedir_profesor():
-      pedir = generar_dict_compras()
+@task
+def pedir_profesor():
+  pedir = generar_dict_compras()
+  liberar_almacen("recepcion")
+  for sku, cantidad in pedir.items():
+      pedir_prod_profesor(sku, cantidad)
       liberar_almacen("recepcion")
-      for sku, cantidad in pedir.items():
-          pedir_prod_profesor(sku, cantidad)
-          liberar_almacen("recepcion")
 
 
 @task
 def cocinar_task():
-    ver_buzon()
+ver_buzon()
 
- @task
- def verificar_task():
-      verificar()
+@task
+def verificar_task():
+  verificar()
